3a1dc4f5f2b125206636c8ea400f3e0cd0fc1fe8
Hakan Aslan <haka0089@edu.easj.dk>
2018-09-16 16:25:48 +0200

    Class car added

---
+++ BilletLibrary/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    public class Car
+    {
+        /// <summary>
+        /// This is a class of a car that we need in order to have a price for this kind og vehicle in our ticket sistem
+        /// </summary>
+        public Car()
+        {
+            
+        }
+
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public decimal Price()
+        {
+            return 240;
+        }
+
+        public string Vehicle()
+        {
+            return "Car";
+        }
+
+
+
+    }
+}
