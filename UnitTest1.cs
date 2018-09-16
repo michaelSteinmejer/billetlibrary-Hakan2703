0bd25499e12bf8088e8b53801bda66d3600e9ef0
Hakan Aslan <haka0089@edu.easj.dk>
2018-09-16 16:55:43 +0200

    ingen ide

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace billetlibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
