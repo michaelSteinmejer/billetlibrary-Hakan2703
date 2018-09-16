728c4d5a9de4c8cd3c5c581af1e3e59cec333822
Hakan Aslan <haka0089@edu.easj.dk>
2018-09-16 17:25:34 +0200

    MC class added

---
+++ BilletLibrary/MC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    class MC
+    {
+
+        public MC()
+        {
+            
+        }
+
+
+        public string Nummerplade { get; set; }
+        public DateTime dato { get; set; }
+
+        public decimal Price()
+        {
+            return 125;
+        }
+
+        public string vehicle()
+        {
+            return "MC";
+        }
+
+
+    }
+}
